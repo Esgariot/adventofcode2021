d7e7cf0b5dad1cc671777d92e472a23c81708a07
Esgariot <esgariot@icloud.com>
2021-12-05 22:17:50 +0100

    Remove redundant.

--- 1/count.hs
+++ 1/count.hs
@@ -2,20 +2,17 @@ module Main where
 import System.Environment
 import System.Exit
 
-count [] = 0
-count [x] = 0
 count (x1:x2:xs)
         | x1 < x2 = 1 + count (x2:xs)
         | otherwise =  count (x2:xs)
+count _ = 0
 
 rollingSum3 (x1:x2:x3:xs) = (x1+x2+x3):rollingSum3 (x2:x3:xs)
-rollingSum3 xs = []
- 
+rollingSum3 _ = []
 
 convert = count . rollingSum3 . map (\l -> read l :: Int) . lines
 
 parse ["-h"] = usage >> exitSuccess 
-parse [] = usage >> exitFailure
 parse [input] = readFile input
 parse _ = usage >> exitFailure 
 
